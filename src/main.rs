05313ab5547e05d893bd057679a842a8b7d05c98
Robert Usher <266585+dcchut@users.noreply.github.com>
2023-02-27 21:49:40 +1100

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ use std::{
 
 use anyhow::{Context, Result};
 use clap::Parser;
-use parser::SyntaxKind::{COMMA, L_PAREN, R_PAREN, WHITESPACE};
+use parser::{
+    SyntaxKind,
+    SyntaxKind::{COMMA, IDENT, L_PAREN, R_PAREN, WHITESPACE},
+};
 use rayon::prelude::*;
 use syntax::{
     algo::diff,
@@ -19,17 +22,19 @@ use syntax::{
     ted::{Element, Position},
     AstNode,
     NodeOrToken::Token,
-    SyntaxToken,
+    SyntaxNode, SyntaxToken, SyntaxTreeBuilder,
 };
 use text_edit::TextEdit;
 
 #[derive(Clone, Debug)]
 struct Component<'a> {
-    ident: &'a str,
+    ident: Option<&'a str>,
     tokens: &'a [SyntaxToken],
 }
 
 impl<'a> Component<'a> {
+    // <LEFT WHITESPACE>std:: fmt:: Debug<RIGHT WHITESPACE>
+    // ^ sep
     pub fn new(tokens: &'a [SyntaxToken]) -> Self {
         let ident = tokens
             .iter()
@@ -40,19 +45,32 @@ impl<'a> Component<'a> {
                     None
                 }
             })
-            .last()
-            .unwrap();
+            .last();
 
-        Self { tokens, ident }
+        Self { ident, tokens }
     }
 }
 
-fn reorder_components(components: &mut [Component<'_>]) {
+fn reorder_components(components: &mut Vec<Component<'_>>) {
+    let fixed_points = components
+        .iter()
+        .enumerate()
+        .filter(|(_, component)| component.ident.is_none())
+        .collect::<Vec<_>>();
+
+    let mut parts = components.to_vec();
+    parts.retain(|x| x.ident.is_some());
+    parts.sort_by_key(|c| c.ident);
+
+    for (i, comp) in fixed_points {
+        parts.insert(i, comp.clone());
+    }
+
+    *components = parts;
     // We may want some sort of grouping behaviour, e.g.;
     // Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord
     //                     <----------->  <------------->
     // For now, just sort alphabetically.
-    components.sort_by_key(|c| std::cmp::Reverse(c.ident));
 }
 
 // TODO: clean up this abomination
@@ -84,52 +102,189 @@ pub fn modify_source(source: &mut String) -> Result<()> {
             }
         }
 
-        // We should have _at least_ ( and ).
-        if tokens.len() < 2
-            || tokens[0].kind() != L_PAREN
-            || tokens.last().unwrap().kind() != R_PAREN
-        {
-            continue;
+        let mut builder = SyntaxTreeBuilder::default();
+        builder.start_node(SyntaxKind::TOKEN_TREE);
+
+        let mut separators: Vec<Vec<SyntaxToken>> = vec![];
+        let mut initial_separator = None;
+        let mut groups = vec![];
+        let mut acc = vec![];
+        for token in &tokens[1..(tokens.len() - 1)] {
+            if token.kind() == COMMA {
+                assert!(!acc.is_empty());
+
+                // Is there any left whitespace?
+                let left_whitespace_idx = acc.iter().position(|x: &SyntaxToken| x.kind() != WHITESPACE && x.kind() != COMMA)
+                    .unwrap_or(0);
+                let right_whitespace_idx = acc.iter().rposition(|x: &SyntaxToken| x.kind() != WHITESPACE && x.kind() != COMMA)
+                    .unwrap_or(acc.len()-1);
+
+                // 0..0 left_whitespace_idx..=right_whitespace_idx right_whitespace_idx..acc.len();
+
+                let l = &acc[0..left_whitespace_idx];
+                let m = &acc[left_whitespace_idx..=right_whitespace_idx];
+                let r = &acc[(right_whitespace_idx+1)..];
+
+                if !l.is_empty() {
+                    // TODO: what if separators is empty?
+                    if separators.is_empty() {
+                        initial_separator = Some(l.to_vec());
+                    } else {
+                        let q = separators.len() - 1;
+                        separators[q].extend_from_slice(l);
+                    }
+                }
+
+                let mut sep = vec![token.clone()];
+                sep.extend_from_slice(r);
+                separators.push(sep);
+                //         let left_whitespace = tokens.split(|t| t.kind() != WHITESPACE && t.kind() != COMMA)
+                //             .next().unwrap();
+                //         let right_whitespace = tokens.rsplit(|t| t.kind() != WHITESPACE && t.kind() != COMMA)
+                //             .next().unwrap();
+
+                groups.push(acc[left_whitespace_idx..=right_whitespace_idx].to_vec());
+                acc.clear();
+                // std::mem::take(&mut acc));
+            } else {
+                acc.push(token.clone());
+            }
+        }
+        if !acc.is_empty() {
+            groups.push(acc);
         }
 
-        let components: Vec<_> = tokens[1..tokens.len() - 1]
-            .split_inclusive(|token| token.kind() == COMMA)
-            .map(Component::new)
+        // Now build components
+        let components: Vec<_> = groups
+            .iter()
+            .map(|group| Component::new(group.as_slice()))
             .collect();
 
         let mut sorted_components = components.clone();
         reorder_components(&mut sorted_components);
 
-        ted::remove_all(
-            tokens[0].clone().syntax_element()..=tokens[tokens.len() - 1].clone().syntax_element(),
-        );
-
-        for (i, component) in sorted_components.into_iter().enumerate() {
-            // Remove whitespace from the start and end.
-            let tt: Vec<_> = component.tokens.to_vec();
-
-            let l = tt.iter().position(|x| x.kind() != WHITESPACE).unwrap();
-            let r = tt
-                .iter()
-                .rposition(|x| x.kind() != COMMA && x.kind() != WHITESPACE)
-                .unwrap();
-
-            let tt: Vec<_> = tt[l..=r].to_vec();
-            let vv: Vec<_> = tt
-                .into_iter()
-                .map(|c| c.syntax_element())
-                .chain(if i > 0 {
-                    Some(Token(token(COMMA)))
-                } else {
-                    None
-                })
-                .collect();
-
-            ted::insert_all(Position::first_child_of(tree.syntax()), vv);
+        if let Some(separators) = initial_separator {
+            for token in separators {
+                builder.token(token.kind(), token.text());
+            }
         }
 
-        ted::insert(Position::first_child_of(tree.syntax()), token(L_PAREN));
-        ted::insert(Position::last_child_of(tree.syntax()), token(R_PAREN));
+        let mut sep_iter = separators.into_iter();
+        for component in sorted_components {
+            for token in component.tokens {
+                builder.token(token.kind(), token.text());
+            }
+            if let Some(sep) = sep_iter.next() {
+                for token in sep {
+                    builder.token(token.kind(), token.text());
+                }
+            }
+        }
+        for sep in sep_iter
+        {
+            for token in sep {
+                builder.token(token.kind(), token.text());
+            }
+        }
+        builder.finish_node();
+
+        let parse = builder.finish().syntax_node().clone_for_update();
+
+        println!("BEFORE:\n{}", tree);
+
+        ted::replace_all(
+            tokens[1].clone().syntax_element()..=tokens[tokens.len() - 2].clone().syntax_element(),
+            vec![parse.syntax_element()],
+        );
+
+        println!("AFTER:\n{}", tree);
+        //
+        // dbg!(parse);
+        //
+        // // L_PAREN@9..10 "(",
+        // //     IDENT@10..15 "Clone",
+        // //     COMMA@15..16 ",",
+        // //     WHITESPACE@16..17 " ",
+        // //     IDENT@17..21 "Copy",
+        // //     COMMA@21..22 ",",
+        // //     WHITESPACE@22..23 " ",
+        // //     IDENT@23..26 "std",
+        // //     WHITESPACE@26..27 " ",
+        // //     COLON@27..28 ":",
+        // //     COLON@28..29 ":",
+        // //     WHITESPACE@29..30 " ",
+        // //     IDENT@30..33 "fmt",
+        // //     WHITESPACE@33..34 " ",
+        // //     COLON@34..35 ":",
+        // //     COLON@35..36 ":",
+        // //     WHITESPACE@36..37 " ",
+        // //     IDENT@37..42 "Debug",
+        // //     COMMA@42..43 ",",
+        // //     WHITESPACE@43..44 " ",
+        // //     IDENT@44..46 "Eq",
+        // //     COMMA@46..47 ",",
+        // //     WHITESPACE@47..48 " ",
+        // //     IDENT@48..52 "Hash",
+        // //     COMMA@52..53 ",",
+        // //     WHITESPACE@53..54 " ",
+        // //     IDENT@54..57 "Ord",
+        // //     COMMA@57..58 ",",
+        // //     WHITESPACE@58..59 " ",
+        // //     IDENT@59..68 "PartialEq",
+        // //     COMMA@68..69 ",",
+        // //     WHITESPACE@69..70 " ",
+        // //     IDENT@70..80 "PartialOrd",
+        // //     R_PAREN@80..81 ")",
+        // //  WHITESPACE@401..406 "\n    ",
+        //
+        // dbg!(&tokens);
+        //
+        // // We should have _at least_ ( and ).
+        // if tokens.len() < 2
+        //     || tokens[0].kind() != L_PAREN
+        //     || tokens.last().unwrap().kind() != R_PAREN
+        // {
+        //     continue;
+        // }
+        //
+        // let components: Vec<_> = tokens[1..tokens.len() - 1]
+        //     .split_inclusive(|token| token.kind() == COMMA)
+        //     .filter_map(Component::new)
+        //     .collect();
+        //
+        // let mut sorted_components = components.clone();
+        // reorder_components(&mut sorted_components);
+        //
+        // // for (before, after) in components.into_iter().zip(sorted_components) {
+        // //     let t: SyntaxToken = todo!();
+        // //
+        // //
+        // //     before.tokens
+        // // }
+        //
+        //
+        // ted::remove_all(
+        //     tokens[0].clone().syntax_element()..=tokens[tokens.len() - 1].clone().syntax_element(),
+        // );
+        //
+        // for (i, component) in sorted_components.into_iter().enumerate() {
+        //     // Remove whitespace from the start and end.
+        //     let tt: Vec<_> = component.tokens.to_vec();
+        //     let vv: Vec<_> = tt
+        //         .into_iter()
+        //         .map(|c| c.syntax_element())
+        //         .chain(if i > 0 {
+        //             Some(Token(token(COMMA)))
+        //         } else {
+        //             None
+        //         })
+        //         .collect();
+        //
+        //     ted::insert_all(Position::first_child_of(tree.syntax()), vv);
+        // }
+        //
+        // ted::insert(Position::first_child_of(tree.syntax()), token(L_PAREN));
+        // ted::insert(Position::last_child_of(tree.syntax()), token(R_PAREN));
 
         let mut builder = TextEdit::builder();
         let d = diff(_tree.syntax(), tree.syntax());
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -6,7 +6,7 @@ use crate::modify_source;
 fn test_derive_ordering() {
     let mut source = dedent(
         r#"
-        #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        #[derive(Eq, Ord, PartialOrd, Copy, Clone, Debug, PartialEq, Hash)]
         struct Wrapped<T>(T);
     "#,
     );
@@ -18,9 +18,45 @@ fn test_derive_ordering() {
 fn test_derive_ordering_qualified() {
     let mut source = dedent(
         r#"
-        #[derive(Clone, Copy, std :: fmt :: Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+        #[derive ( PartialEq, Copy, PartialOrd, std :: fmt :: Debug, Hash, Eq, Ord )]
         struct SillyBugger;
     "#,
+    );
+    modify_source(&mut source).unwrap();
+    insta::assert_snapshot!(source);
+}
+
+#[test]
+fn test_multiline_derives() {
+    let mut source = dedent(
+        r#"
+        #[derive(
+            core::cmp::PartialEq,
+            core::clone::Clone,
+            core::marker::Copy,
+            core::fmt::Debug,
+            core::hash::Hash,
+            core::default::Default,
+            core::cmp::Eq,
+            core::cmp::Ord,
+            core::cmp::PartialOrd,
+        )]
+        struct Core;
+
+        #[derive(
+            std::fmt::Debug,
+            std::clone::Clone,
+            std::marker::Copy,
+            std::cmp::PartialEq,
+            std::default::Default,
+            std::cmp::Eq,
+            std::hash::Hash,
+            std::cmp::Ord,
+            std::cmp::PartialOrd,
+        )]
+        struct Std;
+
+        "#,
     );
     modify_source(&mut source).unwrap();
     insta::assert_snapshot!(source);
